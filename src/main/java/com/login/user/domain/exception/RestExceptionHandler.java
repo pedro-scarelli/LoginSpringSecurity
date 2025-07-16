2e253afb039da557877165d461412079f43b3eb5
Pedro Scarelli <pvscarelli@gmail.com>
2025-07-16 17:21:20 -0300

    feat: improved expection handling

--- src/main/java/com/login/user/domain/exception/RestExceptionHandler.java
+++ src/main/java/com/login/user/domain/exception/RestExceptionHandler.java
@@ -1,65 +1,45 @@
 package com.login.user.domain.exception;
 
-import java.util.Map;
+import com.login.user.domain.model.RestErrorMessage;
+import com.login.user.domain.dto.response.ErrorResponseDTO;
 
 import lombok.NonNull;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.*;
+import org.springframework.web.bind.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.login.user.domain.dto.response.ErrorResponseDTO;
+import jakarta.annotation.PostConstruct;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(UserNotFoundException.class)
-    private ResponseEntity<@NonNull Map<String, String>> userNotFoundHandler(UserNotFoundException exception){
-        var errors = Map.of("message", exception.getMessage());
+    private final Map<Class<? extends RuntimeException>, HttpStatus> exceptionStatusMap = new HashMap<>();
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
+    @PostConstruct
+    private void init() {
+        exceptionStatusMap.put(UserNotFoundException.class, HttpStatus.NOT_FOUND);
+        exceptionStatusMap.put(DuplicateCredentialsException.class, HttpStatus.BAD_REQUEST);
+        exceptionStatusMap.put(IncorrectCredentialsException.class, HttpStatus.BAD_REQUEST);
+        exceptionStatusMap.put(UnauthorizedException.class, HttpStatus.UNAUTHORIZED);
+        exceptionStatusMap.put(UserNotActivatedException.class, HttpStatus.UNAUTHORIZED);
+        exceptionStatusMap.put(EmailMessagingException.class, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(DuplicateCredentialsException.class)
-    private ResponseEntity<@NonNull Map<String, String>> duplicateCredentialsHandler(DuplicateCredentialsException exception){
-        var errors = Map.of("message", exception.getMessage());
- 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    @ExceptionHandler(RuntimeException.class)
+    private ResponseEntity<@NonNull RestErrorMessage> handleKnownExceptions(RuntimeException ex) {
+        HttpStatus status = exceptionStatusMap.getOrDefault(ex.getClass(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return buildResponse(ex, status);
     }
 
-    @ExceptionHandler(IncorrectCredentialsException.class)
-    private ResponseEntity<@NonNull Map<String, String>> incorrectCredentialsHandler(IncorrectCredentialsException exception){
-        var errors = Map.of("message", exception.getMessage());
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-    }
-
-    @ExceptionHandler(UnauthorizedException.class)
-    private ResponseEntity<@NonNull Map<String, String>> unauthorizedExceptionHandler(UnauthorizedException exception){
-        var errors = Map.of("message", exception.getMessage());
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errors);
-    }
-
-    @ExceptionHandler(UserNotActivatedException.class)
-    private ResponseEntity<@NonNull Map<String, String>> userNotActivatedExceptionHandler(UserNotActivatedException exception){
-        var errors = Map.of("message", exception.getMessage());
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errors);
-    }
-
-    @ExceptionHandler(EmailMessagingException.class)
-    private ResponseEntity<@NonNull Map<String, String>> emailMessagingExceptionHandler(EmailMessagingException exception){
-        var errors = Map.of("message", exception.getMessage());
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errors);
+    private ResponseEntity<@NonNull RestErrorMessage> buildResponse(RuntimeException ex, HttpStatus status) {
+        var error = new RestErrorMessage(status, ex.getMessage());
+        return ResponseEntity.status(status).body(error);
     }
 
     @Override
@@ -68,12 +48,12 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         @NonNull HttpHeaders headers,
         @NonNull HttpStatusCode status,
         @NonNull WebRequest request) {
- 
-        var errors = ex.getBindingResult()
+
+        List<String> errors = ex.getBindingResult()
             .getFieldErrors()
             .stream()
             .map(DefaultMessageSourceResolvable::getDefaultMessage)
-            .toList();
+            .collect(Collectors.toList());
 
         var errorResponse = new ErrorResponseDTO(
             HttpStatus.BAD_REQUEST,
@@ -81,6 +61,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
             errors
         );
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
